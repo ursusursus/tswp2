51cd806e265b7d5e132f8e29e39c336b87e771ad
ursusursus <v.brecka@gmail.com>
2013-12-01 21:04:12 +0100

    dasdsa

--- index.php
+++ index.php
@@ -1,27 +1,72 @@
 <?php
-require 'Slim/Slim.php';
 
-\Slim\Slim::registerAutoloader ();
-$app = new \Slim\Slim ();
+//////////// SETUPOVACKY ///////////////////
+session_start();
 
-$app->get("/", function () use($app) {
-	echo "hello world";
+require "Slim/Slim.php";
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+define("HOME", "/tswp2");
+define("FIRST_PAGE", HOME . "/page/1");
+define("LOGIN_PAGE", HOME . "/login");
+
+//////////// ROUTY //////////////////
+
+/**
+* Home
+*/
+$app->get("/", function() use ($app) {
+	checkAuthenticated($app);
+	$app->redirect(FIRST_PAGE);
 });
 
-$app->post("/login", function () use($app) {
-	echo "login";
+/**
+* Login
+*/
+$app->get("/login", function() use ($app) {
+	$app->render("login.html");
 });
 
-
-$app->post("/logout", function () use($app) {
-	echo "logout";
+/**
+* Logout
+*/
+$app->post("/logout", function() use ($app) {
+	session_destroy();
+	$app->redirect(LOGIN_PAGE);
 });
 
-//
-$app->run ();
+/**
+* Actual login
+*/ 
+$app->post("/login", function() use ($app) {
+	$request = $app->request();
+	$username = $request->post("username");
+	$password = $request->post("password");
+
+	if($username == "user" && $password == "password") {
+		$_SESSION["user_id"] = "dummy_user_id";
+		$app->redirect(FIRST_PAGE);
+
+	} else {
+		$app->flash("message", "wrong username or password");
+		$app->redirect(LOGIN_PAGE);
+	}
+});
+
+/**
+* Get page
+*/
+$app->get("/page/:page", function($page) use ($app) {
+	checkAuthenticated($app);
+	$app->render("page.html", array("page" => $page));
+});
+
+///////////// HELPERY ///////////////
 
 function getDatabase() {
-	$config = include ("config.php");
+	$config = include("config.php");
 	$hostname = $config ["hostname"];
 	$dbname = $config ["dbname"];
 	$username = $config ["username"];
@@ -30,3 +75,12 @@ function getDatabase() {
 	$pdo = new PDO ( "mysql:host=$hostname;dbname=$dbname", $username, $password );
 	return $pdo;
 }
+
+function checkAuthenticated($app) {
+    if(!isset($_SESSION["user_id"])) {
+        $app->redirect(LOGIN_PAGE);
+    }
+}
+//////////////////////////////////////
+
+$app->run ();
